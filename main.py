4532d1d3952c9cc20f942c90324351842b21ff42
Valodya_Gevorgyan <gevorgyan.2184575@studenti.uniroma1.it>
2024-11-08 23:05:09 +0100

    First Commit...!

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 #  Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
 #  between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
 
-# L =[]
+L =[]
 
-# for i in range(2000,3201):
-#     if i%7==0 and i%5!=0:
-#         L.append(str(i))
+for i in range(2000,3201):
+    if i%7==0 and i%5!=0:
+        L.append(str(i))
 
-# print(",".join(L))
+print(",".join(L))
 
 # Question: 2
 #  Write a program which can compute the factorial of a given numbers. 
@@ -17,16 +17,16 @@
 #  8 Then, the output should be: 40320
 
 
-# def  fact(x):
-#     if x ==0:
-#         return 1
-#     else:
-#         return x*fact(x-1)
+def  fact(x):
+    if x ==0:
+        return 1
+    else:
+        return x*fact(x-1)
     
 
-# x=int(input("Write a number"))
+x=int(input("Write a number"))
 
-# print(fact(x))
+print(fact(x))
 
 
 # Question: 3 
@@ -36,15 +36,15 @@
 # {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
 
 
-# def abr(n):
-#     d =dict()
-#     for i in range(1,n+1):
-#         d[i] = i*i
-#     return d
+def abr(n):
+    d =dict()
+    for i in range(1,n+1):
+        d[i] = i*i
+    return d
 
-# n =int(input("write a number"))
-# result =abr(n)
-# print(result)
+n =int(input("write a number"))
+result =abr(n)
+print(result)
 
 
 # Question: 4
